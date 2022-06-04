c8c7fb45b3d8e8342ccded721671c5fffe00d8dd
Min RK <benjaminrk@gmail.com>
2022-06-04 10:36:23 +0200

    skip build tests during cross-compilation

--- recipe/build-libdolfinx.sh
+++ recipe/build-libdolfinx.sh
@@ -3,6 +3,10 @@ if [[ "$target_platform" == "osx-64" ]]; then
   export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 fi
 
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" == "1" ]]; then
+  CMAKE_ARGS="${CMAKE_ARGS} -DDOLFINX_SKIP_BUILD_TESTS=1"
+fi
+
 cmake \
   ${CMAKE_ARGS} \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
