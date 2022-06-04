609e2c806c4cf8d0407329bd4d6fd6a81e1e1c6a
Min RK <benjaminrk@gmail.com>
2022-06-04 12:32:09 +0200

    set OPAL_CC, OPAL_PREFIX for cross-compile openmpi
    I don't really understand, but it worked in petsc/slepc

--- recipe/build-dolfinx.sh
+++ recipe/build-dolfinx.sh
@@ -3,4 +3,10 @@ if [[ "$target_platform" == "osx-64" ]]; then
   export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 fi
 
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" == "1" ]]; then
+  # needed for cross-compile openmpi
+  export OPAL_CC="$CC"
+  export OPAL_PREFIX="$PREFIX"
+fi
+
 $PYTHON -m pip install -vv --no-deps ./python
--- recipe/build-libdolfinx.sh
+++ recipe/build-libdolfinx.sh
@@ -5,7 +5,12 @@ fi
 
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" == "1" ]]; then
   CMAKE_ARGS="${CMAKE_ARGS} -DDOLFINX_SKIP_BUILD_TESTS=1"
+  # needed for cross-compile openmpi
+  export OPAL_CC="$CC"
+  export OPAL_PREFIX="$PREFIX"
 fi
+
+
 
 cmake \
   ${CMAKE_ARGS} \
