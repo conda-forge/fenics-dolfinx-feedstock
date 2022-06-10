e6500db226a266e6b08e007237e7a056643922c5
Min RK <benjaminrk@gmail.com>
2022-06-10 12:38:15 +0200

    add complex builds

--- recipe/test-dolfinx.sh
+++ recipe/test-dolfinx.sh
@@ -1,6 +1,5 @@
 set -ex
 
-$PYTHON -c "import dolfinx"
 pip check
 
 # disable clang availability check
@@ -13,6 +12,8 @@ export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
 export OMPI_MCA_rmaps_base_oversubscribe=1
 export OMPI_MCA_plm=isolated
 export OMPI_MCA_btl_vader_single_copy_mechanism=none
+
+pytest -vsx ${RECIPE_DIR}/test_dolfinx.py
 
 cd python/demo
 pytest -vsx -k poisson test.py
---
+++ recipe/test_dolfinx.py
@@ -0,0 +1,29 @@
+import os
+
+import numpy as np
+from petsc4py.PETSc import ScalarType
+import pytest
+
+from dolfinx.cpp import common
+
+
+def test_petsc_scalar():
+    scalar = os.environ["scalar"]
+    is_complex = issubclass(ScalarType, np.complexfloating)
+    if scalar == "complex":
+        assert is_complex
+    else:
+        assert not is_complex
+
+
+@pytest.mark.parametrize(
+    "feature",
+    [
+        "adios2",
+        "parmetis",
+        "slepc",
+        # kahip, # not yet
+    ],
+)
+def test_has(feature):
+    assert getattr(common, f"has_{feature}")
