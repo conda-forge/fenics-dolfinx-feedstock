651a168f9de4852f47dbe9786dfa643e2841bc47
Min RK <benjaminrk@gmail.com>
2022-08-17 11:19:05 +0200

    sync python build patches with upstream
    no changes, only respecting $CMAKE_ARGS and $CMAKE_BUILD_PARALLEL_LEVEL

--- recipe/build-dolfinx.sh
+++ recipe/build-dolfinx.sh
@@ -3,6 +3,11 @@ if [[ "$target_platform" == "osx-64" ]]; then
   export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 fi
 
+if [[ "$target_platform" == "linux-aarch64" || "$target_platform" == "linux-ppc64le" ]]; then
+  # travis aarch/ppc builds run out of memory with parallel builds
+  export CMAKE_BUILD_PARALLEL_LEVEL=1
+fi
+
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" == "1" ]]; then
   # needed for cross-compile openmpi
   export OPAL_CC="$CC"
