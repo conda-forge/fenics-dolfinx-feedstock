d3b77cc8ee57f8277a5afae39190f3186a9902c7
Min RK <benjaminrk@gmail.com>
2024-04-26 12:59:29 +0200

    emulated tests take too long

--- recipe/test-dolfinx.sh
+++ recipe/test-dolfinx.sh
@@ -9,6 +9,7 @@ if [[ "$target_platform" =~ "osx" ]]; then
   export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 fi
 
+export PYTHONUNBUFFERED=1
 export OMPI_ALLOW_RUN_AS_ROOT=1
 export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
 export OMPI_MCA_rmaps_base_oversubscribe=1
@@ -26,7 +27,12 @@ pytest -vs -k demo_poisson test.py
 # run some tests
 cd $TEST_DIR/python/test
 # subset of tests should exercise dependencies, solvers, partitioners
-TESTS="unit/fem/test_fem_pipeline.py unit/mesh/test_mesh_partitioners.py"
+if [[ "$target_platform" == "linux-aarch64" || "$target_platform" == "linux-ppc64le" ]]; then
+  # emulated platforms time out running tests
+  TESTS="unit/fem/test_fem_pipeline.py::test_P_simplex unit/mesh/test_mesh_partitioners.py"
+else
+  TESTS="unit/fem/test_fem_pipeline.py unit/mesh/test_mesh_partitioners.py"
+fi
 # unit/fem/test_fem_pipeline.py::test_dP_simplex[3-DG-tetrahedron] is failing
 # with 4e-6 > 1e-9
 pytest -vs -k 'not test_dP_simplex' $TESTS
