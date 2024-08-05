cbdb85025ad59ca214e634ff0836b33d87c5dd42
Min RK <benjaminrk@gmail.com>
2024-08-05 14:03:02 +0200

    remove outdated ompi env

--- recipe/test-dolfinx.sh
+++ recipe/test-dolfinx.sh
@@ -10,12 +10,6 @@ if [[ "$target_platform" =~ "osx" ]]; then
 fi
 
 export PYTHONUNBUFFERED=1
-export OMPI_ALLOW_RUN_AS_ROOT=1
-export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
-export OMPI_MCA_rmaps_base_oversubscribe=1
-export OMPI_MCA_plm=isolated
-export OMPI_MCA_btl=tcp,self
-export OMPI_MCA_btl_vader_single_copy_mechanism=none
 
 # test packaging
 pytest -vs test_dolfinx.py
--- recipe/test-libdolfinx.sh
+++ recipe/test-libdolfinx.sh
@@ -10,13 +10,6 @@ if [[ "$target_platform" =~ "osx" ]]; then
   export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 fi
 
-export OMPI_ALLOW_RUN_AS_ROOT=1
-export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
-export OMPI_MCA_rmaps_base_oversubscribe=1
-export OMPI_MCA_plm=isolated
-export OMPI_MCA_btl_vader_single_copy_mechanism=none
-export OMPI_MCA_btl=tcp,self
-
 cmake -DCMAKE_BUILD_TYPE=Developer -B build-test/ -S cpp/test/
 cmake --build build-test --parallel "${CPU_COUNT:-1}" --verbose
 cd build-test
