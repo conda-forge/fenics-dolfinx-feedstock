0e76069052d932d9159f31bdd27bf86a68cb89dd
Min RK <benjaminrk@gmail.com>
2024-08-06 08:42:42 +0200

    set no-array-bounds

--- recipe/test-libdolfinx.sh
+++ recipe/test-libdolfinx.sh
@@ -9,6 +9,12 @@ ffcx cpp/test/poisson.py -o cpp/test
 if [[ "$target_platform" =~ "osx" ]]; then
   export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 fi
+if [[ "$target_platform" =~ "linux" ]]; then
+  # array bounds check seems to be needed for some versions of gcc
+  # (only linux cross-compile at the moment)
+  # ref: https://github.com/FEniCS/dolfinx/pull/3216/files#diff-1bba462ab050e89360fd88110a689e85ee037749cea091a1848ab574381d3795R260-R265
+  export CXXFLAGS="${CXXFLAGS} -Wno-array-bounds"
+fi
 
 cmake -DCMAKE_BUILD_TYPE=Developer -B build-test/ -S cpp/test/
 cmake --build build-test --parallel "${CPU_COUNT:-1}" --verbose
