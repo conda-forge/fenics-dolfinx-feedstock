671d823bbefdaa61f5563294598d203cdbb2f618
Min RK <benjaminrk@gmail.com>
2026-02-21 09:36:15 -0800

    delete symlink in build, not patch

--- recipe/build-libdolfinx.bat
+++ recipe/build-libdolfinx.bat
@@ -3,7 +3,7 @@ setlocal EnableDelayedExpansion
 
 :: can't have symlinks in windows package, delete unused file
 :: that rattler-build installs under test-files
-del %PREFIX%/etc/conda/test-files/fenics-libdolfinx/0/cpp/test/vcpkg.json
+del cpp/test/vcpkg.json
 
 set "CXXFLAGS=%CXXFLAGS% -DH5_BUILT_AS_DYNAMIC_LIB"
 
